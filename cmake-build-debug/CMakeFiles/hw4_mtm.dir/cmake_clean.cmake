955a825be426a1a87c814e265170b430d1d6d5e4
IdanCohen <36813204+idan94@users.noreply.github.com>
2018-05-31 13:21:04 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/hw4_mtm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw4_mtm.dir/main.cpp.obj"
+  "hw4_mtm.pdb"
+  "hw4_mtm.exe"
+  "hw4_mtm.exe.manifest"
+  "libhw4_mtm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw4_mtm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Check!" << std::endl;
+    int i(2);
+    std::cout << i << std::endl;
+    return 0;
+}
